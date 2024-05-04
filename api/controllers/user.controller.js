92ddb75cf4c5df824e3c72cfe76ae39f1ce50afc
nitindahiya199 <nitindahiya00000@gmail.com>
2024-05-04 23:11:11 +0530

    update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,59 @@
-export const test = (req,res)=>{
-    res.status(200).json({message: "Api is working ?>?"})
+import { bcryptjs } from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import { User } from "../modals/user.modal.js";
+
+
+export const test = (req, res) => {
+  res.status(200).json({ message: "Api is working ?>?" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
+  }
+  if (req.body.password) {
+    if (req.body.password < 6) {
+      return next(errorHandler(400, "Password must be at least 6 characters "));
+    }
+    res.body.password = bcryptjs.hashSync(res.body.password, 10);
+  }
+
+  if (req.body.username.length < 7 && req.body.username.length > 20) {
+    return next(
+      errorHandler(400, "Username must be between 7 and 20 characters")
+    );
+  }
+
+  if (req.body.username.include(" ")) {
+    return next(errorHandler(400, "Username cannot contain spaces"));
+  }
+
+  if (req.body.username !== req.body.username.toLowerCase()) {
+    return next(errorHandler(400, "Username must be lowercase"));
+  }
+
+  if (req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+    return next(
+      errorHandler(400, "Username can only contain letters and numbers")
+    );
+  }
+
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
