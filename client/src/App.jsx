54413a604c7b5db8a23cf82861e1688ac31c7ba4
nitindahiya199 <nitindahiya00000@gmail.com>
2024-05-14 00:17:09 +0530

    update post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePosts';
 // import UpdatePost from './pages/UpdatePost';
 // import PostPage from './pages/PostPage';
 // import ScrollToTop from './components/ScrollToTop';
@@ -31,11 +32,11 @@ export default function App() {
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
-          {/* <Route path='/update-post/:postId' element={<UpdatePost />} /> */}
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
         </Route>
 
-        {/* <Route path='/projects' element={<Projects />} />
-        <Route path='/post/:postSlug' element={<PostPage />} /> */}
+         <Route path='/projects' element={<Projects />} />
+      {/*  <Route path='/post/:postSlug' element={<PostPage />} /> */}
       </Routes>
       <Footer />
     </BrowserRouter>
