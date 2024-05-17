77953ffaa19ed696df0dcbcff77e6e82e241f249
nitindahiya199 <nitindahiya00000@gmail.com>
2024-05-17 21:28:19 +0530

    add comment + users in dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,8 @@ import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
+import DashComments from '../components/DashComments';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -19,6 +21,8 @@ export default function Dashboard() {
       </div>
       {tab === 'profile' && <DashProfile />}
       {tab === 'posts' && <DashPosts />}
+      {tab === 'users' && <DashUsers />}
+      {tab === 'comments' && <DashComments />}
     </div>
   )
 }
