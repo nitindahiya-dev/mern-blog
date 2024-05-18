407bca38bb8706bba7e86651659be6d537fd6ccc
nitindahiya199 <nitindahiya00000@gmail.com>
2024-05-19 00:40:03 +0530

    add other pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,26 @@
-
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Mern' Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sint ex aliquam eveniet assumenda magnam quam nostrum praesentium modi odit, odio magni similique tempora nobis autem dicta! Voluptas, aliquid error!
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur enim rerum accusamus perspiciatis voluptas quam, illum minima cupiditate officia modi voluptatibus unde sunt sit consequuntur ipsum doloremque, aut aliquam error iste cumque facilis animi? Incidunt!
+            </p>
+
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex, aliquam?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
